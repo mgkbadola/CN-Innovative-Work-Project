29e955183bbf24c2663d9dcc8e9e0f588a6b154e
Mrigank Badola <badola3@gmail.com>
2020-11-17 22:51:52 +0530

    now gives points by taking a difference of length of longest word and entered one.

--- client.py
+++ client.py
@@ -1,30 +1,29 @@
-#!/usr/bin/python3
 import socket as s
 import random
 
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-socket.connect((s.gethostname(), 1024))
+socket.connect(('127.0.0.1', 80))
 letters = ''
-vowels='eeeeeeeeeeeeaaaaaaaaaiiiiiiiiioooooooouuuu'
-consonants='nnnnnnrrrrrrttttttllllssssddddgggbbccmmppffhhvvwwyykjxqz'
+vowels = 'eeeeeeeeeeeeaaaaaaaaaiiiiiiiiioooooooouuuu'
+consonants = 'nnnnnnrrrrrrttttttllllssssddddgggbbccmmppffhhvvwwyykjxqz'
 while letters == '':
-    i=0
+    i = 0
     while i < 6:
         print('Vowel(0) or Consonant(1)?', end=' ')
         option = int(input())
         if option == 0:
-            ind=random.randint(0, len(vowels)-1)
+            ind = random.randint(0, len(vowels)-1)
             letters += vowels[ind]
         else:
-            ind=random.randint(0, len(consonants)-1)
+            ind = random.randint(0, len(consonants)-1)
             letters += consonants[ind]
         i += 1
     print(f"Letters: {letters}")
-    socket.send(bytes(letters,"utf-8"))
-    #pass1
-    print("What's your word?",end=' ')
+    socket.send(bytes(letters, "utf-8"))
+    # pass1
+    print("What's your word?", end=' ')
     word = input()
-    socket.send(bytes(word,"utf-8"))
-    #pass2
+    socket.send(bytes(word, "utf-8"))
+    # pass2
     message = socket.recv(1024).decode("utf-8")
     print(message)
--- server.py
+++ server.py
@@ -1,10 +1,57 @@
-#!/usr/bin/python3
 import socket as s
-import random
-import re
+
+
+class Dictionary:
+    def __init__(self):
+        self.data = {
+            "a": list(),
+            "b": list(),
+            "c": list(),
+            "d": list(),
+            "e": list(),
+            "f": list(),
+            "g": list(),
+            "h": list(),
+            "i": list(),
+            "j": list(),
+            "k": list(),
+            "l": list(),
+            "m": list(),
+            "n": list(),
+            "o": list(),
+            "p": list(),
+            "q": list(),
+            "r": list(),
+            "s": list(),
+            "t": list(),
+            "u": list(),
+            "v": list(),
+            "w": list(),
+            "x": list(),
+            "y": list(),
+            "z": list(),
+            "all": list()
+        }
+
+    def insert(self, char, word_inp):
+        self.data[char].append(word_inp)
+
+    def refer(self, char):
+        return self.data[char]
+
+
+dictionary = Dictionary()
+f = open('words.txt', 'r')
+while True:
+    line = f.readline().replace('\n', '')
+    if line:
+        dictionary.insert(line[0], line)
+        dictionary.insert("all", line)
+    else:
+        break
 
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-socket.bind((s.gethostname(), 1024))
+socket.bind(('127.0.0.1', 80))
 socket.listen(5)
 client_open = False
 
@@ -12,29 +59,45 @@ while True:
     client, address = socket.accept()
     client_open = True
     print(f"Connected to {address}")
-    
+
     while client_open:
         message = client.recv(1024).decode("utf-8")
         letters = set(message)
-        
+
         size = 0
         word = client.recv(1024).decode("utf-8")
-        wordset = set(word)
-        
-        #implement dictionary checking
-        isword = random.randint(0,1)
-        if isword == 1:
-            for value in letters:
-                regex = re.search(f'[a-zA-Z]*{value}[a-zA-Z]*',word)
-                if regex:
-                    size += 1
+        word_set = set(word)
+
+        maxima = 0
+        max_word = ''
+        for words in dictionary.refer("all"):
+            S = set(words)
+            if S.issubset(letters):
+                if maxima < len(words):
+                    maxima = len(words)
+                    max_word = words
+
+        dict_fail = False
+        is_in_dict = False
+        if word in dictionary.refer(word[0]):
+            if word_set.issubset(letters):
+                is_in_dict = True
         else:
-            client.send(bytes('It is not a valid word','utf-8'))
-        
-        if size == len(wordset):
-            client.send(bytes("x points to you, well done!",'utf-8'))
+            if max_word != '':
+                client.send(bytes('It is not a valid word', 'utf-8'))
+            else:
+                client.send(bytes('It is not possible to make a valid word', 'utf-8'))
+                dict_fail = True
+        # no word can be built
+        if dict_fail:
+            client.send(bytes("Hence this round won't be counted!"))
         else:
-            client.send(bytes('Your word failed the regex check','utf-8'))
+            # if the word is in dictionary
+            if is_in_dict:
+                client.send(bytes(f"{len(max_word)-len(word)} points to you, well done! "
+                                  f"Longest word: {max_word}", 'utf-8'))
+            else:
+                client.send(bytes('Your word wasn\'t present in the dictionary', 'utf-8'))
 
         client_open = False
     
