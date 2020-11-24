32adbfa8f7f332ec49bfd9db361d65f6c07850fc
Mrigank Badola <badola3@gmail.com>
2020-11-24 13:10:18 +0530

    updated server.py

--- server.py
+++ server.py
@@ -4,33 +4,10 @@ import socket as s
 class Dictionary:
     def __init__(self):
         self.data = {
-            "a": list(),
-            "b": list(),
-            "c": list(),
-            "d": list(),
-            "e": list(),
-            "f": list(),
-            "g": list(),
-            "h": list(),
-            "i": list(),
-            "j": list(),
-            "k": list(),
-            "l": list(),
-            "m": list(),
-            "n": list(),
-            "o": list(),
-            "p": list(),
-            "q": list(),
-            "r": list(),
-            "s": list(),
-            "t": list(),
-            "u": list(),
-            "v": list(),
-            "w": list(),
-            "x": list(),
-            "y": list(),
-            "z": list(),
-            "all": list()
+            "a": list(), "b": list(), "c": list(), "d": list(), "e": list(), "f": list(), "g": list(), "h": list(),
+            "i": list(), "j": list(), "k": list(), "l": list(), "m": list(), "n": list(), "o": list(), "p": list(),
+            "q": list(), "r": list(), "s": list(), "t": list(), "u": list(), "v": list(), "w": list(), "x": list(),
+            "y": list(), "z": list(), "all": list()
         }
 
     def insert(self, char, word_inp):
@@ -53,17 +30,14 @@ while True:
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 socket.bind(('127.0.0.1', 80))
 socket.listen()
-client_open = False
 
 while True:
     client, address = socket.accept()
-    client_open = True
     print(f"Connected to {address}")
 
-    while client_open:
+    while True:
         message = client.recv(1024).decode("utf-8")
         if message == "2":
-            client_open = False
             break
         letters = set(message)
         if ' ' in letters:
@@ -72,7 +46,6 @@ while True:
         size = 0
         word = client.recv(1024).decode("utf-8")
         if word == "2":
-            client_open = False
             break
         word_set = set(word)
 
@@ -86,7 +59,8 @@ while True:
                     max_word = words
 
         if max_word == '':
-            client.send(bytes('It is not possible to make a valid word!Hence this round won\'t be counted', 'utf-8'))
+            client.send(bytes('It is not possible to make a valid word!'
+                              'Hence this round won\'t be counted', 'utf-8'))
         else:
             if word in dictionary.refer(word[0]):
                 if word_set.issubset(letters):
@@ -98,7 +72,5 @@ while True:
             else:
                 client.send(bytes(f'You scored 0/{len(max_word)}!Your word wasn\'t present in the dictionary!'
                                   f'Longest word: {max_word}', 'utf-8'))
-
-        # client_open = False
 
     client.close()
